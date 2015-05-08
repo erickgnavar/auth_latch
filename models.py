b23adfd8a3c20a339f2483820cbb1cd34ea0febe
Erick Navarro <erick@navarro.io>
2015-05-08 10:04:23 -0500

    added unpair method and fixed error when latch is not correctly configured

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 from openerp import models, fields, api
-from openerp.exceptions import MissingError, ValidationError, AccessDenied
+from openerp.exceptions import ValidationError, AccessDenied
 
 from latch import latch
 
@@ -8,8 +8,8 @@ from latch import latch
 def _get_api(obj):
     app_id = obj.env['ir.config_parameter'].get_param('latch.app.id')
     secret_key = obj.env['ir.config_parameter'].get_param('latch.secret.key')
-    if not app_id or not secret_key:
-        raise MissingError('Latch application setup incomplete')
+    if not app_id or not secret_key:  # TODO: add log entry
+        return False
     return latch.Latch(app_id, secret_key)
 
 
@@ -17,7 +17,7 @@ def _old_get_api(obj, cr, uid):
     app_id = obj.pool['ir.config_parameter'].get_param(cr, uid, 'latch.app.id')
     secret_key = obj.pool['ir.config_parameter'].get_param(cr, uid, 'latch.secret.key')
     if not app_id or not secret_key:
-        raise MissingError('Latch application setup incomplete')
+        return False
     return latch.Latch(app_id, secret_key)
 
 
@@ -26,6 +26,7 @@ class User(models.Model):
     _inherit = 'res.users'
 
     latch_account_id = fields.Char('Latch account id')
+    latch_paired = fields.Boolean(default=False)
 
     @api.v7
     def check_credentials(self, cr, uid, password):
@@ -35,13 +36,28 @@ class User(models.Model):
     @api.v7
     def _check_latch(self, cr, uid):
         _api = _old_get_api(self, cr, uid)
-        user = self.read(cr, uid, uid, ['latch_account_id'])
+        if not _api:
+            return
+        user = self.read(cr, uid, uid, ['latch_paired', 'latch_account_id'])
+        if not user['latch_account_id'] or not user['latch_account_id']:
+            return
         response = _api.status(user['latch_account_id'])
         # FIXME: find a better way to check by operations
         data = response.get_data()
         status = data['operations'][data['operations'].keys()[0]]['status']
         if status == 'off':
             raise AccessDenied()
+
+    @api.one
+    def latch_unpair(self):
+        _api = _get_api(self)
+        if not _api:
+            return
+        _api.unpair(self.latch_account_id)
+        self.env.user.write({
+            'latch_account_id': '',
+            'latch_paired': False
+        })
 
 
 class PairLatchAccountWizard(models.TransientModel):
@@ -53,9 +69,12 @@ class PairLatchAccountWizard(models.TransientModel):
     @api.one
     def pair(self):
         _api = _get_api(self)
+        if not _api:
+            return
         response = _api.pair(self.pairing_code)
         if 'accountId' not in response.get_data():
             raise ValidationError('A problem with pairing process occurred')
         self.env.user.write({
-            'latch_account_id': response.get_data()['accountId']
+            'latch_account_id': response.get_data()['accountId'],
+            'latch_paired': True
         })
